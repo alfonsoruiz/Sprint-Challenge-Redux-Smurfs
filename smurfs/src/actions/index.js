cbe0f732f4049311c1065d5f0409f94245a3a196
Alfonso Ruiz <1800ruiz@gmail.com>
2019-06-30 18:56:52 -0700

    initialized project, setup redux store. Created actions and reducers for get request for smurfs. Created components and connected map state to props

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,8 +1,7 @@
-/* 
-  Action Types Go Here!
-  Be sure to export each action type so you can pull it into your reducer
-*/
+import axios from 'axios';
 
+export const FETCH_SMURFS = 'FETCH_SMURFS';
+export const FETCH_SMURFS_FAILURE = 'FETCH_SMURFS_FAILURE';
 /*
   For this project you'll need at least 2 action creators for the main portion,
    and 2 more for the stretch problem.
@@ -13,3 +12,13 @@
    U - updateSmurf
    D - deleteSmurf
 */
+
+export const getSmurfs = () => dispatch => {
+  axios.get('http://localhost:3333/smurfs')
+    .then(res => {
+      dispatch({ type: FETCH_SMURFS, payload: res.data });
+    })
+    .catch(err => {
+      dispatch({ type: FETCH_SMURFS_FAILURE, payload: err});
+    })
+}
--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,18 +1,14 @@
-/*
-  Be sure to import in all of the action types from `../actions`
-*/
+import { FETCH_SMURFS } from '../actions';
+import { FETCH_SMURFS_FAILURE } from '../actions';
 
-/*
- Your initial/default state for this project could *Although does not have to* look a lot like this
- {
+ const initialState = {
    smurfs: [],
-   fetchingSmurfs: false
-   addingSmurf: false
-   updatingSmurf: false
-   deletingSmurf: false
+   fetchingSmurfs: false,
+   addingSmurf: false,
+   updatingSmurf: false,
+   deletingSmurf: false,
    error: null
  }
-*/
 
 /*
   You'll only need one smurf reducer for this project.
@@ -21,3 +17,22 @@
   There is no need for 'combineReducers' in this project.
   Components can then read your store as, `state` and not `state.fooReducer`.
 */
+
+
+export const reducer = (state = initialState, action) => {
+  switch(action.type) {
+    case FETCH_SMURFS:
+      return {
+        ...state,
+        smurfs: action.payload,
+        fetchingSmurfs: true
+      };
+    case FETCH_SMURFS_FAILURE:
+      return {
+        ...state,
+        error: action.payload
+      }
+    default:
+      return state;
+  }
+}
